70b55d8a96b0d8544f9c514af4f45c8493b7fda1
hanishaputlur2006-cloud <hanishaputlur2006@gmail.com>
2025-11-18 23:43:47 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
 
+app.get('/uploads', (req, res) => {
+  res.sendFile(__dirname + '/uploads.html');
+});
+
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
     if (username === 'admin' && password === 'admin123') {
